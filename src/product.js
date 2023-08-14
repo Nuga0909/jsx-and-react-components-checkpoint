5babf6030564a176bb9ea683d8e620a03c5593a2
Adeshina Adenuga <Nuga0909@gmail.com>
2023-08-14 04:53:23 +0100

    create stringified product info

---
+++ src/product.js
@@ -0,0 +1,12 @@
+const Product = {
+  name: "Casio Vintage Wrist Watch",
+  price: "₦13,899",
+  description:
+    "Unisex classic wrist watch Enhances every style, it make statements with accessories that symbolize style. Casio Authentic Wrist Watch is an elegantly designed wrist watch . Its superior quality and versatile design make it fit for all occasions. It is made from premium steel so it's real value for your money.",
+  imageUrl:
+    "https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/31/4127612/1.jpg?6951",
+};
+
+const ProductStrf = JSON.stringify(Product);
+
+export default ProductStrf;
